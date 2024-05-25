bf2613bc4b355532063eb3e18138582d7dec7436
MelikaEl <melika.melika8@gmail.com>
2024-05-25 10:33:40 +0430

    storage

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -8,9 +8,18 @@ function Cuisine() {
   let params = useParams();
 
   const getCuisine = async (name) => {
-    const data = await fetch(
-      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
-    );
+    const check = localStorage.getItem("cuisine");
+    if (check) {
+      setCuisine(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
+      );
+      const data = await api.json();
+      localStorage.setItem("cuisine", JSON.stringify(data.recipes));
+      setCuisine(data.recipes);
+      console.log(data.recipes);
+    }
 
     const recipes = await data.json();
     setCuisine(recipes.results); //results dar inga esme gharardadi ast ke dar site spoonacular, backend on ra tarif karde
